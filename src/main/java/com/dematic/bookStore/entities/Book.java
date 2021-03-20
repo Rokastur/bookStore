4c079acc660d88defb91b7d6f76349a8f9fe4cda
Rokas Atminas <rokasatminas@gmail.com>
2021-03-20 17:53:28 +0200

    added annotations to avoid conflicts in db

--- src/main/java/com/dematic/bookStore/entities/Book.java
+++ src/main/java/com/dematic/bookStore/entities/Book.java
@@ -11,6 +11,7 @@ import java.util.Set;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "Book_Type")
+@DiscriminatorValue("Book")
 public abstract class Book {
 
     @Id
@@ -21,6 +22,7 @@ public abstract class Book {
 
     protected Integer quantity;
 
+    @Column(name = "unit_price")
     protected BigDecimal unitPrice;
 
     @ManyToMany
