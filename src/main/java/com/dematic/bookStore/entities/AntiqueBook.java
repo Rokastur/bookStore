c9ff8df6bfdf3ced1f444f2d86128d5e4dbb25a3
Rokas Atminas <rokasatminas@gmail.com>
2021-03-20 19:36:55 +0200

    added missing annotation

--- src/main/java/com/dematic/bookStore/entities/AntiqueBook.java
+++ src/main/java/com/dematic/bookStore/entities/AntiqueBook.java
@@ -5,10 +5,12 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
 @DiscriminatorValue("AntiqueBook")
 public class AntiqueBook extends Book {
 
