cd5db36821fea4b2aef24a9ab149cbe7302c4720
Rokas Atminas <rokasatminas@gmail.com>
2021-03-20 18:22:52 +0200

    added explicit name for column

--- src/main/java/com/dematic/bookStore/entities/AntiqueBook.java
+++ src/main/java/com/dematic/bookStore/entities/AntiqueBook.java
@@ -3,15 +3,16 @@ package com.dematic.bookStore.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
 import java.time.LocalDate;
 
 @Getter
 @Setter
-@Entity
 @DiscriminatorValue("AntiqueBook")
 public class AntiqueBook extends Book {
 
+    @Column(name = "release_year")
     private LocalDate releaseYear;
+
 }
