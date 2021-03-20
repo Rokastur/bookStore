2bc0bba2d5d572ea6bf028c2c8c743b6493cbf33
Rokas Atminas <rokasatminas@gmail.com>
2021-03-20 15:39:14 +0200

    created a service class for base entity Book

---
+++ src/main/java/com/dematic/bookStore/services/BookService.java
@@ -0,0 +1,14 @@
+package com.dematic.bookStore.services;
+
+import com.dematic.bookStore.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+}
