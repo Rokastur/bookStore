cbbf84238501bb2ff05420be1ffd8e8d6ff4cb78
Rokas Atminas <rokasatminas@gmail.com>
2021-03-20 16:56:47 +0200

    Wrote a  method with a custom query to retrieve barcodes for the books in stock ordered by quantity

--- src/main/java/com/dematic/bookStore/repositories/BookRepository.java
+++ src/main/java/com/dematic/bookStore/repositories/BookRepository.java
@@ -1,9 +1,15 @@
 package com.dematic.bookStore.repositories;
 
 import com.dematic.bookStore.entities.Book;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface BookRepository extends CrudRepository<Book, String> {
+
+    @Query(value = "SELECT Barcode FROM BOOK WHERE quantity > 0 ORDER BY COUNT DESC", nativeQuery = true)
+    Set<String> findAllBarcodesOrderByNonNullQuantityDesc();
 }
