ff56f331e766a84c2ccf17fbb053429cae9ff7b8
Rokas Atminas <rokasatminas@gmail.com>
2021-03-20 13:48:59 +0200

    Initial commit

---
+++ src/test/java/com/dematic/bookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dematic.bookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
