9b1cc98f83c24b07e0e7bf7e8275536bcaf6eaff
Rokas Atminas <rokasatminas@gmail.com>
2021-03-23 19:31:10 +0200

    added method to return Optional value of Author

--- src/main/java/com/dematic/bookStore/repositories/AuthorRepository.java
+++ src/main/java/com/dematic/bookStore/repositories/AuthorRepository.java
@@ -4,9 +4,14 @@ import com.dematic.bookStore.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     boolean existsByNameAndLastName(String name, String lastName);
+
     Author getOneByNameAndLastName(String name, String lastName);
+
+    Optional<Author> findOneByNameAndLastName(String name, String lastName);
 }
