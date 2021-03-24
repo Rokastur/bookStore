f91fcde26ba16fed0a548811f708039c1d21b349
Rokas Atminas <rokasatminas@gmail.com>
2021-03-24 23:14:19 +0200

    query now maps to List instead of Set

--- src/main/java/com/dematic/bookStore/repositories/BookRepository.java
+++ src/main/java/com/dematic/bookStore/repositories/BookRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Set;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, String> {
 
     @Query(value = "SELECT Barcode FROM BOOK WHERE quantity > 0 ORDER BY QUANTITY DESC", nativeQuery = true)
-    Set<String> findAllBarcodesOrderByNonNullQuantityDesc();
+    List<String> findAllBarcodesOrderByNonNullQuantityDesc();
 
     @Query(value = "SELECT Barcode FROM BOOK WHERE book_type =:bookType", nativeQuery = true)
     Set<String> findAllBarcodesByBookType(String bookType);
