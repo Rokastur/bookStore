225d516fed030183c332cfcc3feec0fb49fb9030
Rokas Atminas <rokasatminas@gmail.com>
2021-03-27 21:20:01 +0200

    removed redundant annotation

--- src/main/java/com/dematic/bookStore/repositories/AuthorRepository.java
+++ src/main/java/com/dematic/bookStore/repositories/AuthorRepository.java
@@ -2,11 +2,9 @@ package com.dematic.bookStore.repositories;
 
 import com.dematic.bookStore.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     Optional<Author> findOneByNameAndLastName(String name, String lastName);
