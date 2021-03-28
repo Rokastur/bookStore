afe865095eec6273f1e2f1431c8ebc0f3c4fc6ac
Rokas Atminas <rokasatminas@gmail.com>
2021-03-28 15:31:20 +0300

    added new validation constraints which are only triggered when using @Validated(NewEntity.class)

--- src/main/java/com/dematic/bookStore/controller/utility/BookAuthorDTO.java
+++ src/main/java/com/dematic/bookStore/controller/utility/BookAuthorDTO.java
@@ -25,18 +25,30 @@ public class BookAuthorDTO {
      */
     public static final String ISBNRegex = "^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$";
 
-    @Pattern(regexp = ISBNRegex, message = "Invalid ISBN [DTO]")
+    private final String genericMessage = "cannot create new publication without providing ";
+
+    @Pattern(groups = {NewEntity.class}, regexp = ISBNRegex, message = "Invalid ISBN")
+    @NotNull(groups = {NewEntity.class}, message = genericMessage + "barcode")
     private String barcode;
+
+    @NotEmpty(groups = {NewEntity.class}, message = genericMessage + "title")
     private String title;
+
     @PositiveOrZero(message = "book must have a quantity of 0 or more")
+    @NotNull(groups = {NewEntity.class}, message = genericMessage + "quantity")
     private Integer quantity;
+
     @PositiveOrZero(message = "book must have a unit price value of 0 or more")
+    @NotNull(groups = {NewEntity.class}, message = genericMessage + "unitPrice")
     private BigDecimal unitPrice;
+
     @Range(min = 1, max = 10, message = "science index must be between 1 and 10, inclusive")
     private Integer scienceIndex;
+
     @PastOrPresent(message = "only released books may be added")
     private LocalDate releaseYear;
 
+    @NotEmpty(groups = {NewEntity.class}, message = genericMessage + "at least one author")
     private Set<AuthorDTO> authorsDTO = new HashSet<>();
 
     @Getter
